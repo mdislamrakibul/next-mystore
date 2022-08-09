44660294c2fa74d002b93974b521ffa9b626c17b
rakib-developer <rakibul.nexkraft@gmail.com>
2022-08-09 17:47:54 +0600

    dsign

--- components/admin/AdminLayout.js
+++ components/admin/AdminLayout.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import React from 'react';
 import Modal from '../Modal';
 import NavBar from '../Navbar';
+import Sidebar from './component/Sidebar';
 
 function AdminLayout({ children })
 {
@@ -19,65 +20,7 @@ function AdminLayout({ children })
             <Modal />
             <div className='row'>
                 <div className='col-md-4'>
-                    <div className="flex-shrink-0 p-3 bg-white" style={{ width: '280px' }}>
-                        <a href="/" className="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
-                            <span className="fs-5 fw-semibold">Admin Panel</span>
-                        </a>
-                        <ul className="list-unstyled ps-0">
-                            <li className="mb-1">
-                                <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-                                    Home
-                                </button>
-                                <div className="collapse show" id="home-collapse">
-                                    <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Overview</a></li>
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Updates</a></li>
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Reports</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li className="mb-1">
-                                <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-                                    Dashboard
-                                </button>
-                                <div className="collapse" id="dashboard-collapse">
-                                    <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Overview</a></li>
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Weekly</a></li>
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Monthly</a></li>
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Annually</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li className="mb-1">
-                                <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-                                    Orders
-                                </button>
-                                <div className="collapse" id="orders-collapse">
-                                    <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">New</a></li>
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Processed</a></li>
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Shipped</a></li>
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Returned</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li className="border-top my-3"></li>
-                            <li className="mb-1">
-                                <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-                                    Account
-                                </button>
-                                <div className="collapse" id="account-collapse">
-                                    <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">New...</a></li>
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Profile</a></li>
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Settings</a></li>
-                                        <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Sign out</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+                    <Sidebar />
                 </div>
                 <div className='col-md-8'>
                     {children}
---
+++ components/admin/component/Sidebar.js
@@ -0,0 +1,71 @@
+import Link from 'next/link'
+import React from 'react'
+function Sidebar()
+{
+    return (
+        <>
+            <div className="flex-shrink-0 p-3 bg-white" style={{ width: '280px' }}>
+                <Link href="/admin/dashboard" >
+                    <a className="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+                        <span className="fs-5 fw-semibold">Admin Panel</span>
+                    </a></Link>
+                <ul className="list-unstyled ps-0">
+                    <li className="mb-1">
+                        <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+                            <i class="fas fa-home"></i>&nbsp;Home
+                        </button>
+                        <div className="collapse show" id="home-collapse" style={{ marginLeft: '50px' }}>
+                            <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Overview</a></li>
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Updates</a></li>
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Reports</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li className="mb-1">
+                        <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+                            <i class="fas fa-tachometer-alt"></i>&nbsp;Dashboard
+                        </button>
+                        <div className="collapse" id="dashboard-collapse" style={{ marginLeft: '50px' }}>
+                            <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Overview</a></li>
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Weekly</a></li>
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Monthly</a></li>
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Annually</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li className="mb-1">
+                        <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+                            <i class="far fa-file-archive"></i>&nbsp;Orders
+                        </button>
+                        <div className="collapse" id="orders-collapse" style={{ marginLeft: '50px' }}>
+                            <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">New</a></li>
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Processed</a></li>
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Shipped</a></li>
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Returned</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li className="border-top my-3"></li>
+                    <li className="mb-1">
+                        <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+                            Account
+                        </button>
+                        <div className="collapse" id="account-collapse" style={{ marginLeft: '50px' }}>
+                            <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">New...</a></li>
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Profile</a></li>
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Settings</a></li>
+                                <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Sign out</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Sidebar
