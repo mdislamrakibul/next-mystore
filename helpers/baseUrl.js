0d4cba41ed16dd6bc2db10af28e91943b901ddc8
rakib-developer <rakibul.nexkraft@gmail.com>
2022-09-08 14:44:15 +0600

    versal upload 5

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,2 +1,2 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? "https://mid-store.vercel.app" : "http://localhost:2222"
+const baseUrl = process.env.NODE_ENV === 'production' ? "https://next-mystore.vercel.app" : "http://localhost:2222"
 export default baseUrl
